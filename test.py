1d82582a7e7c6efd9652083207ecaf54b6d671ce
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-11 23:21:39 +0800

    started test cases

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+import board
+import numpy as np
+
+class TestBoard(unittest.TestCase):
+    def __init__(self):
+        self.board = np.zeros
+    def test_right(self):
+        self.assertEqual(Board())
