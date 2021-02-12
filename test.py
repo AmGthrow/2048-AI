c35d26539596cdc70c993933dc6221ae83d8ae39
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-12 19:56:47 +0800

    Added test case for move_right

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
 import unittest
-import board
+from board import Board
 import numpy as np
 
 class TestBoard(unittest.TestCase):
-    def __init__(self):
-        self.board = np.zeros
+
     def test_right(self):
-        self.assertEqual(Board())
+        start = np.array([
+        [2,0,0,0],
+        [2,0,0,0],
+        [0,0,0,0],
+        [0,4,4,0]], 
+        dtype=int)
+
+        end = np.array([
+        [2,0,0,0],
+        [2,0,0,0],
+        [0,0,0,0],
+        [8,0,0,0]], 
+        dtype=int)
+
+        right_board = Board(start)
+        right_board.move_right()
+        np.testing.assert_equal(right_board.board, end)
+
+
+if __name__ == "__main__":
+    unittest.main()
