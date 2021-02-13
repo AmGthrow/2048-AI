27a0ba3e025fef04d557117d9bce56946a3e2229
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-13 15:20:07 +0800

    added shift_left() to Board

--- board.py
+++ board.py
@@ -27,5 +27,17 @@ class Board:
     def move_right(self):
         pass
 
+    def shift_left(self):
+        """Shifts every tile as far left as it could go without merging anything
+        """
+        for row in range(self.rows):
+            move_to = 0  # tracks which col a tile to move should be moved to
+            for col in range(self.cols):
+                if self.board[row][col] != 0:
+                    # swap col and move_to
+                    # You might be alarmed that we're swapping but since this only happens if col == 0, this is
+                    # more like a "bubble sort" where 0s get bubbled up
+                    self.board[row][move_to], self.board[row][col] self.board[row][col], self.board[row][move_to]
+                    move_to += 1
 if __name__ == "__main__":
     board = Board()
