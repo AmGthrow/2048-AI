211a9e5740771c46816b526c679c44e0899b645f
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-13 16:06:33 +0800

    added tests for 'move_()' functions

--- test.py
+++ test.py
@@ -2,28 +2,86 @@ import unittest
 from board import Board
 import numpy as np
 
+
 class TestBoard(unittest.TestCase):
 
     def test_left(self):
         """Test to make sure move_left() both shifts all elements and merges similar values
         """
         start = np.array([
-        [2,0,0,0],
-        [2,0,0,0],
-        [0,0,0,0],
-        [0,4,4,0]], 
-        dtype=int)
+            [2, 0, 0, 0],
+            [2, 2, 0, 0],
+            [0, 4, 4, 4],
+            [0, 4, 4, 0]],
+            dtype=int)
 
         end = np.array([
-        [2,0,0,0],
-        [2,0,0,0],
-        [0,0,0,0],
-        [8,0,0,0]], 
-        dtype=int)
+            [2, 0, 0, 0],
+            [4, 0, 0, 0],
+            [8, 4, 0, 0],
+            [8, 0, 0, 0]],
+            dtype=int)
 
         left_board = Board(start)
         left_board.move_left()
         np.testing.assert_equal(left_board.board, end)
+
+    def test_right(self):
+        start = np.array([
+            [2, 0, 0, 0],
+            [2, 2, 0, 0],
+            [0, 4, 4, 4],
+            [0, 4, 4, 0]],
+            dtype=int)
+
+        end = np.array([
+            [0, 0, 0, 2],
+            [0, 0, 0, 4],
+            [0, 0, 4, 8],
+            [0, 0, 0, 8]],
+            dtype=int)
+
+        right_board = Board(start)
+        right_board.move_right()
+        np.testing.assert_equal(right_board.board, end)
+
+    def test_up(self):
+        start = np.array([
+            [2, 0, 0, 0],
+            [2, 2, 0, 0],
+            [0, 4, 4, 4],
+            [0, 4, 4, 0]],
+            dtype=int)
+
+        end = np.array([
+            [4, 2, 8, 4],
+            [0, 8, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]],
+            dtype=int)
+
+        up_board = Board(start)
+        up_board.move_up()
+        np.testing.assert_equal(up_board.board, end)
+
+    def test_down(self):
+        start = np.array([
+            [2, 0, 0, 0],
+            [2, 2, 0, 0],
+            [0, 4, 4, 4],
+            [0, 4, 4, 0]],
+            dtype=int)
+
+        end = np.array([
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [4, 8, 8, 4]],
+            dtype=int)
+
+        down_board = Board(start)
+        down_board.move_down()
+        np.testing.assert_equal(down_board.board, end)
 
 
 if __name__ == "__main__":
