2c3df739493d1ca1026e8685f1a75f59d296965d
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-13 15:40:08 +0800

    fixed missing assignment operator

--- board.py
+++ board.py
@@ -3,6 +3,7 @@ import numpy as np
 DEFAULT_ROWS = 4
 DEFAULT_COLS = 4
 
+
 class Board:
     def __init__(self, board=np.zeros((DEFAULT_ROWS, DEFAULT_COLS), dtype=int)):
         self.board = board
@@ -43,7 +44,7 @@ class Board:
                     # swap col and move_to
                     # You might be alarmed that we're swapping but since this only happens if col == 0, this is
                     # more like a "bubble sort" where 0s get bubbled up
-                    self.board[row][move_to], self.board[row][col] self.board[row][col], self.board[row][move_to]
+                    self.board[row][move_to], self.board[row][col] = self.board[row][col], self.board[row][move_to]
                     move_to += 1
 
     def merge_left(self):
