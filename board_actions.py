b17a9f538bf1eb952a525b00fdd2dadff6183c1d
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-15 21:56:07 +0800

    Added option to specify num_moves and num_trials in BoardDriver.perform_best_move()

--- board_actions.py
+++ board_actions.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import re
 import AI
 
+DEFAULT_NUM_MOVES = 3
+DEFAULT_NUM_TRIALS = 200
 
 class BoardDriver:
     def __init__(self, browser):
@@ -90,12 +92,12 @@ class BoardDriver:
     def move_down(self):
         self.html.send_keys(Keys.DOWN)
 
-    def perform_best_move(self, board):
+    def perform_best_move(self, board, num_moves = DEFAULT_NUM_MOVES, num_trials = DEFAULT_NUM_TRIALS):
         function_names = {
             'ai_up': self.move_up,
             'ai_down': self.move_down,
             'ai_left':self.move_left,
             'ai_right':self.move_right
         }
-        move = AI.get_best_move(board, 3, 200)
+        move = AI.get_best_move(board, num_moves, num_trials)
         function_names[move.__name__]()
