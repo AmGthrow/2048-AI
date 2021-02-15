df579082cb18f58cd5795e29a1ba508544712a67
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-15 21:41:46 +0800

    Log board when winning and losing

--- play.py
+++ play.py
@@ -6,6 +6,11 @@ from selenium.webdriver.common.keys import Keys
 
 import time
 from board_actions import BoardDriver
+import logging
+
+logging.basicConfig(filename='BoardDriver.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s', encoding="utf-8", level=logging.INFO)
+
 
 def play():
     # Takes the browser to play2048.co and starts a game
@@ -17,19 +22,22 @@ def play():
     board = BoardDriver(browser)
     # sends keys in the sequence UP, DOWN, LEFT, RIGHT and restarts the game when the option appears
     while True:
-        #TODO: Log the best move?
+        # TODO: Log the best move?
         board.perform_best_move(board.get_tiles())
-        
-        # it takes a while for the HTML to update with the board, so I need to wait a bit. Otherwise, get_tiles() 
+
+        # it takes a while for the HTML to update with the board, so I need to wait a bit. Otherwise, get_tiles()
         # gives a wrong board, one where the move/s might not have been done yet
         time.sleep(0.1)
-        print(board.get_tiles())
+        logging.debug(f"New board: {board.get_tiles()}")
 
         # Try to click "Keep Going" if it shows up
         try:
-            # TODO: Log the board at every game win
             # Press the "Keep Going" button that shows up when we reach the 2048 tile
-            continueGame = browser.find_element_by_class_name('keep-playing-button')
+            continueGame = browser.find_element_by_class_name(
+                'keep-playing-button')
+            # Assuming we reached 2048, log the board's current state
+            logging.info("REACHED 2048")
+            logging.info(str(board.get_tiles()))
             continueGame.click()
         except:
             continue
@@ -37,9 +45,14 @@ def play():
         # If there's no "Keep Going" but retry-button shows up, press that instead
         # Since it means we lost and the board is currently in a Game Over state
         try:
-            # TODO: Log the board at every game over
             # Reset the game when we lose
             resetGame = browser.find_element_by_class_name('retry-button')
+            # Assuming we found the resetGame button, log the board and score
+            logging.info("GAME OVER")
+            logging.info(str(board.get_tiles()))
+            score = browser.find_element_by_class_name('score-container')
+            # Reset the game
+            logging.info(f"SCORE: {score.text}")
             resetGame.click()
         except:
             continue
