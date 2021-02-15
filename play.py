517ddd079712107689dd1c93a96eaf552cce5b8e
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-15 21:23:00 +0800

    Made first selenium-controlled game

--- play.py
+++ play.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 import time
 from board_actions import BoardDriver
+from board import Board
 
 def play():
     # Takes the browser to play2048.co and starts a game
@@ -17,10 +18,9 @@ def play():
     board = BoardDriver(browser)
     # sends keys in the sequence UP, DOWN, LEFT, RIGHT and restarts the game when the option appears
     while True:
-        board.move_up()
-        board.move_left()
-        board.move_down()
-        board.move_right()
+        print("found board")
+        board.perform_best_move(board.get_tiles())
+        print("DID BEST MOVE")
         
         # it takes a while for the HTML to update with the board, so I need to wait a bit. Otherwise, get_tiles() 
         # gives a wrong board, one where the move/s might not have been done yet
@@ -28,7 +28,9 @@ def play():
         print(board.get_tiles())
         try:
             resetGame = browser.find_element_by_class_name('retry-button')
-            resetGame.click()
+            if not Board(board).is_valid():
+                x = input("Game over: ")
+                resetGame.click()
         except:
             continue
 
